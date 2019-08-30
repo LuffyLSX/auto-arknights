9a639315081e0774e3fb933c3a6724cc7aeddbef
Luffy_LSX <754057419@qq.com>
2019-08-30 18:03:26 +0800

    防止升级使脚本失效

--- auto_game.py
+++ auto_game.py
@@ -16,35 +16,47 @@ def screenshot():
     os.system('adb shell screencap /data/screen.png')
     os.system('adb pull /data/screen.png %s' % path)
 
-def Image_to_position(image):
+def Image_to_position(image, m = 0):
     image_path = 'images/' + str(image) + '.png'
-    screenshot()
     screen = cv2.imread('images/screen.png', 0)
     template = cv2.imread(image_path, 0)
-    methods = cv2.TM_CCOEFF_NORMED
+    methods = [cv2.TM_CCOEFF_NORMED, cv2.TM_SQDIFF_NORMED, cv2.TM_CCORR_NORMED]
     image_x, image_y = template.shape[:2]
-    result = cv2.matchTemplate(screen, template, methods)
+    result = cv2.matchTemplate(screen, template, methods[m])
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    print(max_val)
     if max_val > 0.8:
+        global center
         center = (max_loc[0] + image_y / 2, max_loc[1] + image_x / 2)
-        print(center)
-        click(center[0], center[1])
-        return True
+        return center
     else:
         return False
+    
+def run(n):
+    images = ['start-go1', 'start-go2', 'end', 'level up']
+    round = 0
+    # Image_to_position('start-go1')
+    # time.sleep(2)
+    # Image_to_position('start-go2')
+    # while not Image_to_position('end'):
+    #     time.sleep(5)
+    while True:
+        screenshot()
+        for image in images:
+            if Image_to_position(image) != False:
+                print(image)
+                click(center[0], center[1])
 
-def run():
-    Image_to_position('start-go1')
-    time.sleep(1)
-    Image_to_position('start-go2')
-    while not Image_to_position('end'):
-        time.sleep(5)
-
+        if Image_to_position('end') != False :
+            round = round + 1
+            if round == n:
+                break
+        
 
 if __name__ == '__main__':
     connect()
-    for i in range(int(input('输入刷图次数'))):
+    '''for i in range(int(input('输入刷图次数' + '\n'))):
         run()
-        time.sleep(3)
+        time.sleep(3)'''
+    run(int(input('输入刷图次数' + '\n')))
+
         
